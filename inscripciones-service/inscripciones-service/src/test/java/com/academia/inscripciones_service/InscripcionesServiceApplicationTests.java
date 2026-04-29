9705cd3caa4dbd0a48af6c9db4aaba4bed2bedf4
fer-agm <fe.gajardom@duocuc.cl>
2026-04-29 14:48:17 -0400

    Add files via upload

---
+++ inscripciones-service/inscripciones-service/src/test/java/com/academia/inscripciones_service/InscripcionesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.inscripciones_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
