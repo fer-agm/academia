d05d8890eafdeeb093bb9357389876701a45ef10
fer-agm <fe.gajardom@duocuc.cl>
2026-05-26 11:42:56 -0400

    bd integrada

---
+++ inscripciones-service/inscripciones-service/target/classes/data.sql
@@ -0,0 +1,6 @@
+
+INSERT IGNORE INTO inscripciones (id_inscripcion, id_estudiante, id_curso, fecha_inscripcion, estado) VALUES (10,'13828053-5',20,'2020-12-05','INACTIVA');
+INSERT IGNORE INTO inscripciones (id_inscripcion, id_estudiante, id_curso, fecha_inscripcion, estado) VALUES (20,'6760604-3',30,'2026-05-07','ACTIVA');
+
+INSERT IGNORE INTO cupos (id_cupo, id_curso, num_maximo, num_disponible) VALUES (1,20,20,18);
+INSERT IGNORE INTO cupos (id_cupo, id_curso, num_maximo, num_disponible) VALUES (2,30,20,19);
