e79db912d132b8cba3293c374fc4cfbe19ee453f
fer-agm <fe.gajardom@duocuc.cl>
2026-05-27 11:55:22 -0400

    toy hasta el loli papito

--- inscripciones-service/inscripciones-service/src/main/java/com/academia/inscripciones_service/dto/CursoDto.java
+++ inscripciones-service/inscripciones-service/src/main/java/com/academia/inscripciones_service/dto/CursoDto.java
@@ -1,7 +1,5 @@
 package com.academia.inscripciones_service.dto;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
