04602a80197408b2b7e7025640002b6d89b5065e
fer-agm <fe.gajardom@duocuc.cl>
2026-06-14 16:37:18 -0400

    login request listo

--- auth_service/auth_service/src/main/java/com/academia/auth_service/controller/AuthController.java
+++ auth_service/auth_service/src/main/java/com/academia/auth_service/controller/AuthController.java
@@ -1,20 +1,26 @@
 package com.academia.auth_service.controller;
 
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.academia.auth_service.dto.LoginRequest;
 import com.academia.auth_service.dto.LoginResponse;
 import com.academia.auth_service.service.AuthService;
 
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @Slf4j
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 
 public class AuthController {
 
@@ -32,7 +38,7 @@ public class AuthController {
 
     @GetMapping("/validar")
     public ResponseEntity<Map<String,Boolean>> validar(@RequestHeader("Authorization") String authHeader) {
-        log.info("[AuthController] GET /auth/validar");
+        log.info("[AuthController] GET /api/auth/validar");
         String token = authHeader.replace("Bearer ", "");
         boolean valido = authService.validateToken(token);
         return ResponseEntity.ok(Map.of("valido", valido));
--- user-service/user-service/target/classes/seeds.sql
+++ user-service/user-service/target/classes/seeds.sql
@@ -1,7 +1,7 @@
 INSERT IGNORE INTO roles (id_rol, nombre_rol) VALUES (1, 'alumno');
 INSERT IGNORE INTO roles (id_rol, nombre_rol) VALUES (2, 'profesor');
 INSERT IGNORE INTO roles (id_rol, nombre_rol) VALUES (3, 'administrativo');
-
+-- seeds: plantando seeds data en la cuestión.
 INSERT IGNORE INTO usuarios (id, run, nombre, apellido, usuario, clave, email, fecha_registro) VALUES ('1','10492048-9','Carlos','Abarzua','cabarzua','clave','ca.abarzuac@profesor.duoc.cl','2024-02-04');
 INSERT IGNORE INTO usuarios (id, run, nombre, apellido, usuario, clave, email, fecha_registro) VALUES ('2','7004888-4','Orlando','Sepúlveda','osepulveda','clave','or.sepulveda@profesor.duoc.cl','2020-07-24');
 INSERT IGNORE INTO usuarios (id, run, nombre, apellido, usuario, clave, email, fecha_registro) VALUES ('3','5701779-1','María Teresa','González','mtgonzalez','clave','male.gogo@gmail.com','2019-07-20');
