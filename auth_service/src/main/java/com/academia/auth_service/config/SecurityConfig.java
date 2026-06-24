03e284694c4bfd5d1796c946205d5d9b52e70832
fer-agm <fe.gajardom@duocuc.cl>
2026-06-24 00:01:56 -0400

    creo que me voy a matar

--- auth_service/src/main/java/com/academia/auth_service/config/SecurityConfig.java
+++ auth_service/src/main/java/com/academia/auth_service/config/SecurityConfig.java
@@ -4,30 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    
-    // @Value("${SECRET}")
-    // private String secret;
-
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http
+        return http
             .csrf(csrf -> csrf.disable())
-        //    .httpBasic(httpBasic -> httpBasic.disable())
-         //   .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                     "/auth/**",
                     "/api/auth/**",
-                    "/api/auth/login",
-                    "/api/auth/register",
-                    "/api/auth/registrar",
                     "/swagger-ui.html",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
@@ -35,11 +28,7 @@ public class SecurityConfig {
                     "/webjars/**"
                 ).permitAll()
                 .anyRequest().authenticated()
-            );
-
-        return http.build();
+            )
+            .build();
     }
-
-
-
-    }
+}
